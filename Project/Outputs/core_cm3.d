40c0d4f2e9e871399e9379b4a3a6dd714eeffdf0
eastmoutain <east.moutain.yang@gmail.com>
2014-03-26 00:50:23 +0800

    remove some object files and link files
    Signed-off-by: eastmoutain <east.moutain.yang@gmail.com>

--- Project/Outputs/core_cm3.d
+++
@@ -1,2 +0,0 @@
-.\Outputs\core_cm3.o: ..\Libraries\CMSIS\CoreSupport\core_cm3.c
-.\Outputs\core_cm3.o: D:\Program Files\Keil\ARM\RV31\Inc\stdint.h
--- Project/Outputs/startup_stm32f10x_hd.d
+++
@@ -1,1 +0,0 @@
-.\Outputs\startup_stm32f10x_hd.o: ..\Libraries\CMSIS\DeviceSupport\Startup\startup_stm32f10x_hd.s
